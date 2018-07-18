ea7849b06dbb0f50e27efee3d009bcad34aeb471
David Ma <davidma415@gmail.com>
2018-07-18 12:53:33 -0400

    Finished third step of stretch 2

--- stretch2.rb
+++ stretch2.rb
@@ -13,5 +13,23 @@ def get_absent_dogs(arr)
   end
   return absent_dogs
 end
+
+def call_absent_dogs(arr)
+  abs_dogs = get_absent_dogs(arr)
+  abs_dogs.each do |x|
+    puts "Come back, #{x}"
+  end
+end
+
 absent_dogs = get_absent_dogs(my_dogs)
-puts absent_dogs
+
+#2. Calling dogs that are absent. We are able to tell which dogs are absent by calling our get absent dogs method from inside
+call_absent_dogs(my_dogs)
+
+#3.
+neighbour_dogs = [
+  { :name => 'Arnie', :position => 12 },
+  { :name => 'Chowder', :position => 8 },
+  { :name => 'Pup', :position => 15 },
+]
+call_absent_dogs(neighbour_dogs)
